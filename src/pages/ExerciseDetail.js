34d4f1464c4e7482e1a082487a4b2cc7cf161e77
sumit27x <sumitgupta27x@gmail.com>
2024-03-07 12:37:26 +0530

    first commit

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
-
-import { exerciseOptions, fetchData , youtubeOptions} from '../utils/fetchData';
+import { exerciseOptions, fetchData , youtubeOptions } from '../utils/fetchData';
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
